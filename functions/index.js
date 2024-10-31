a6e1e05b9f4cc72a4f0f0465ea88e2dcd957acfa
jihwannnn <kimjih1130@gmail.com>
2024-10-31 19:29:29 +0900

    Add .runtimeconfig.json

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,77 @@
-    /**
-     * Import function triggers from their respective submodules:
-     *
-     * const {onCall} = require("firebase-functions/v2/https");
-     * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
-     *
-     * See a full list of supported triggers at https://firebase.google.com/docs/functions
-     */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require('axios');
 
-    const {onRequest} = require("firebase-functions/v2/https");
-    const logger = require("firebase-functions/logger");
+// Firebase 초기화
+admin.initializeApp();
 
-    //Create and deploy your first functions
-    //https://firebase.google.com/docs/functions/get-started
+// 환경 변수에서 CLIENT_ID와 CLIENT_SECRET 가져오기
+const config = functions.config();
 
-    exports.helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
+const CLIENT_ID = config.spotify.client_id;
+const CLIENT_SECRET = config.spotify.client_secret;
+
+if (!CLIENT_ID || !CLIENT_SECRET) {
+  throw new Error('Spotify CLIENT_ID and CLIENT_SECRET are not set in the environment variables.');
+}
+
+
+// Spotify 토큰을 가져오는 함수
+async function getSpotifyToken() {
+  const url = "https://accounts.spotify.com/api/token";
+  const data = new URLSearchParams({ grant_type: 'client_credentials' });
+  const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
+
+  try {
+    const response = await axios.post(url, data.toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${auth}`
+      }
     });
+
+    if (response.status === 200) {
+      return response.data.access_token;
+    } else {
+      throw new Error(`Failed to get token: ${response.data}`);
+    }
+  } catch (error) {
+    throw new Error(`Failed to get token: ${error.response.data}`);
+  }
+}
+
+// Cloud Function 정의
+exports.getSpotifyTrack = functions.https.onRequest(async (req, res) => {
+  const track_id = req.query.track_id || '1ygmHMAn6HYtCrQ4fHqD0x';
+
+  try {
+    const token = await getSpotifyToken();
+    const headers = { "Authorization": `Bearer ${token}` };
+    const url = `https://api.spotify.com/v1/tracks/${track_id}`;
+
+    const response = await axios.get(url, { headers });
+
+    if (response.status === 200) {
+      const track = response.data;
+
+      // 트랙 정보 추출
+      const track_info = {
+        "Track Name": track.name,
+        "Artist": track.artists[0].name,
+        "Album": track.album.name,
+        "Release Date": track.album.release_date,
+        "Spotify URL": track.external_urls.spotify,
+        "Album Cover": track.album.images[0].url,
+        "Popularity": track.popularity,
+        "Duration (ms)": track.duration_ms
+      };
+
+      // JSON 응답 반환
+      res.status(200).json(track_info);
+    } else {
+      res.status(500).send(`Spotify API Error: ${response.data}`);
+    }
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+});
